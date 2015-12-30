436d29f58f2fc84db9b340ea3580e42b81b6fa3e
KadeHels <kadeiszman@gmail.com>
2015-12-30 14:51:08 -0600

    Update files and functionality added MessageSystem and tried to clean up the mainActivity page

--- app/src/main/java/kadehelseth/test/MainActivity.java
+++ app/src/main/java/kadehelseth/test/MainActivity.java
@@ -1,30 +1,54 @@
 package kadehelseth.test;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AnalogClock;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+import kadehelseth.test.TimeText.Message;
+import kadehelseth.test.TimeText.MessageSystem;
+import kadehelseth.test.TimeText.Time;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        this.system = new MessageSystem();
+
+        final EditText edt = (EditText)findViewById(R.id.messageText);
+        final EditText phoneNumb = (EditText)findViewById(R.id.PhoneNumberField);
+        final EditText time = (EditText)findViewById(R.id.timeCollector);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Snackbar.make(view, "TimeWarped!", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                String messageText = edt.getText().toString();
+                String phoneNumber = phoneNumb.getText().toString();
+                //fixme
+                Time sendTime = new Time(Calendar.getInstance().getTimeInMillis());
+                //end fixme
+                Message textMessage = new Message(messageText, phoneNumber, sendTime);
+                system.addMessage(textMessage);
+                system.sendMessage();
+
             }
         });
     }
@@ -50,4 +74,14 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    /**
+     * adds the message to the messageSystem
+     */
+    private boolean delaySend(Message message){
+
+        return false;
+    }
+    MessageSystem system;
 }
